38b5e662a431189cd91017b8739d3a7b0acb7e6d
sn0wcat <sn0wcat@users.noreply.github.com>
2019-04-28 01:19:37 +0200

    feat(sdk) : iot-file

--- src/api/sdk/iotfile/iot-file.ts
+++ src/api/sdk/iotfile/iot-file.ts
@@ -1,3 +1,20 @@
-import { SdkClient } from "../sdk-client";
+import { SdkClient } from "..";
+import { checkAssetId } from "../../utils";
+import { IotFileModels } from "./iot-file-models";
 
-export class IotFileClient extends SdkClient {}
+export class IotFileClient extends SdkClient {
+    private _baseUrl: string = "/api/iotfile/v3";
+
+    public async GetFiles(
+        entityid: string,
+        optional?: { offset?: number; limit?: number; count?: number; order?: string; filter?: string }
+    ): Promise<IotFileModels.File[]> {
+        checkAssetId(entityid);
+        return (await (this.HttpAction({
+            verb: "GET",
+            baseUrl: `${this._baseUrl}/files/${entityid}`,
+            additionalHeaders: optional,
+            message: "SearchFiles"
+        }) as unknown)) as IotFileModels.File[];
+    }
+}
