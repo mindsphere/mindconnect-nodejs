29b5bac9907214c0e2c5dc61543acf74d13ea9ad
sn0wcat <sn0wcat@users.noreply.github.com>
2019-07-11 00:29:17 +0200

    feat: download-file (#66)
    * feat: mc download-file command
    
    * wip: work in progress
    
    * fix: fixed snyk scripts for windows development
    
    * feat: download-file
    
    * feat: mc download-file
    
    * fix: proper import for the utils
    
    * chore: cleaned up code

---
+++ src/cli/commands/mc-download-file.ts
@@ -0,0 +1,114 @@
+import { CommanderStatic } from "commander";
+import { log } from "console";
+import * as fs from "fs";
+import * as path from "path";
+import * as util from "util";
+import { MindSphereSdk } from "../../api/sdk/";
+import { checksumFile, decrypt, loadAuth, retry, throwError } from "../../api/utils";
+import { errorLog, getColor, humanFileSize, serviceCredentialLog, verboseLog } from "./command-utils";
+import ora = require("ora");
+const streamPipeline = util.promisify(require("stream").pipeline);
+
+const color = getColor("magenta");
+
+export default (program: CommanderStatic) => {
+    program
+        .command("download-file")
+        .alias("df")
+        .option("-f, --file <fileToDownload>", "file to download from the file service")
+        .option("-h, --filepath [filepath]", "file path in the mindsphere", "")
+        .option("-i, --assetid <assetid>", "asset id from the mindsphere")
+        .option("-p, --passkey <passkey>", `passkey`)
+        .option("-y, --retry <number>", "retry attempts before giving up", 3)
+        .option("-v, --verbose", "verbose output")
+        .description(`${color("download the file from mindsphere file service *")}`)
+        .action(options => {
+            (async () => {
+                try {
+                    checkParameters(options);
+
+                    const auth = loadAuth();
+                    const sdk = new MindSphereSdk({
+                        gateway: auth.gateway,
+                        basicAuth: decrypt(auth, options.passkey),
+                        tenant: auth.tenant
+                    });
+                    const iotFileClient = sdk.GetIoTFileClient();
+                    let fullpath = options.filepath ? `${options.filepath}/${options.file}` : `${options.file}`;
+                    fullpath = fullpath.replace("//", "/");
+
+                    let filter = `name eq ${path.basename(fullpath)}`;
+                    if (path.dirname(fullpath) !== ".") filter += ` and path eq ${path.dirname(fullpath)}/`;
+
+                    const fileInfo = await iotFileClient.GetFiles(options.assetid, {
+                        filter: filter
+                    });
+
+                    fileInfo.length !== 1 &&
+                        errorLog(`There were ${color(fileInfo.length)} files found with that name and path.`, true);
+
+                    const spinner = ora("downloading file");
+                    !options.verbose && spinner.start();
+
+                    verboseLog(JSON.stringify(fileInfo, null, 2), options.verbose);
+                    verboseLog(
+                        `Downloading file ${color(fileInfo[0].name)} with size of ${color(
+                            humanFileSize(fileInfo[0].size || 0)
+                        )} from MindSphere.`,
+                        options.verbose,
+                        spinner
+                    );
+
+                    const startDate = new Date();
+
+                    const download = await retry(options.retry, () => iotFileClient.GetFile(options.assetid, fullpath));
+                    !download.ok && throwError(`Unexpected response ${download.statusText}`);
+                    const outputfile = fileInfo[0].name || "output.bin";
+                    const file = fs.createWriteStream(outputfile);
+                    await streamPipeline(download.body, file);
+
+                    const endDate = new Date();
+
+                    !options.verbose && spinner.succeed("Done");
+
+                    const hash = await checksumFile("md5", outputfile);
+
+                    log(`Download time: ${(endDate.getTime() - startDate.getTime()) / 1000} seconds`);
+                    log(`\nYour file ${color(outputfile)} with md5 hash ${hash} was successfully downloaded`);
+                } catch (err) {
+                    errorLog(err, options.verbose);
+                }
+            })();
+        })
+        .on("--help", () => {
+            log("\n  Examples:\n");
+            log(
+                `    mc download-file -f CHANGELOG.md  --assetid 5..f  \t\t\t\t download file ${color(
+                    "CHANGELOG.md"
+                )} from specified asset`
+            );
+            log(
+                `    mc download-file --file  CHANGELOG.md  --assetid 5...f --filepath upload \t download file ${color(
+                    "upload/CHANGELOG.md"
+                )} from specified asset`
+            );
+            log(
+                `    mc download-file --file  upload/CHANGELOG.md  --assetid 5...f \t\t download file ${color(
+                    "upload/CHANGELOG.md"
+                )} from specified asset`
+            );
+            serviceCredentialLog();
+        });
+};
+
+function checkParameters(options: any) {
+    !options.passkey &&
+        errorLog(
+            "you have to provide a passkey to get the service token (run mc df --help for full description)",
+            true
+        );
+    !options.file &&
+        errorLog("Missing file name for download-file command. Run mc df --help for full syntax and examples.", true);
+
+    !options.assetid && errorLog(" You have to specify assetid. Run  mc df --help for full syntax and examples.", true);
+}
