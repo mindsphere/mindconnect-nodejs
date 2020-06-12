4a673dfe2c9f5f02e231fd1494017183b47f915b
sn0wcat <sn0wcat@users.noreply.github.com>
2020-06-12 20:24:12 +0200

    feat: mc dev-proxy

--- src/cli/commands/mc-proxy.ts
+++ src/cli/commands/mc-proxy.ts
@@ -4,13 +4,14 @@ import * as http from "http";
 import * as https from "https";
 import * as url from "url";
 import { MindSphereSdk } from "../../api/sdk";
-import { authJson, decrypt, loadAuth } from "../../api/utils";
+import { authJson, decrypt, loadAuth, throwError } from "../../api/utils";
 import { errorLog, getColor, homeDirLog, proxyLog } from "./command-utils";
 const HttpsProxyAgent = require("https-proxy-agent");
 
 const color = getColor("magenta");
 const red = getColor("red");
 const yellow = getColor("yellow");
+const green = getColor("green");
 
 const headers = {
     "Access-Control-Allow-Origin": "*",
@@ -18,8 +19,7 @@ const headers = {
     "Access-Control-Max-Age": 2592000, // 30 days
     "transfer-encoding": "",
     "cache-control": "no-cache",
-    "x-proxied-by": "@mindconnect/mindconnect-nodejs development proxy",
-    /** add other headers too */
+    "x-proxied-by": "mindsphere development proxy",
 };
 
 export default (program: CommanderStatic) => {
@@ -27,22 +27,32 @@ export default (program: CommanderStatic) => {
         .command("dev-proxy")
         .alias("px")
         .option("-o, --port <port>", "port for web server", 7707)
+        .option("-c, --nocors", "don't provide cors headers")
+        .option("-r, --norewrite", "don't rewrite hal+json urls")
+        .option("-w, --nowarn", "don't warn for missing headers")
         .option("-v, --verbose", "verbose output")
         .option("-k, --passkey <passkey>", "passkey")
-        .description(color("starts mindsphere development proxy"))
+        .description(color("starts mindsphere development proxy *"))
         .action((options) => {
             (async () => {
                 try {
                     homeDirLog(options.verbose, color);
                     proxyLog(options.verbose, color);
 
-                    const auth = loadAuth();
+                    checkRequiredParamaters(options);
 
+                    const auth = loadAuth();
                     const sdk = new MindSphereSdk({ ...auth, basicAuth: decrypt(auth, options.passkey) });
 
-                    await serve({ auth, configPort: options.port, sdk });
+                    console.log(`\nCORS support ${options.nocors ? red("off") : green("on")}`);
+                    console.log(
+                        `Rewrite hal+json support ${auth.gateway} -> ${"http://localhost:" + options.port} ${
+                            options.norewrite ? red("off") : green("on")
+                        }`
+                    );
 
-                    console.log(auth);
+                    console.log(`warn on missing x-xsrf-token ${options.nowarn ? red("off") : green("on")}\n`);
+                    await serve({ auth, configPort: options.port, sdk, options });
                 } catch (err) {
                     errorLog(err, options.verbose);
                 }
@@ -50,22 +60,45 @@ export default (program: CommanderStatic) => {
         })
         .on("--help", () => {
             log("\n  Examples:\n");
-            log(`    mc dev-proxy \t\t\t starts mindsphere development proxy on default port (7707)`);
-            log(`\n    mc dev-proxy --port 7777 \t\t\t starts mindsphere development proxy on port 7777`);
+            log(
+                `    mc dev-proxy --passkey <yourpasskey> \t\t\t starts mindsphere development proxy on default port (7707)`
+            );
+            log(
+                `    mc dev-proxy --port 7777 --passkey <yourpasskey> \t\t starts mindsphere development proxy on port 7777`
+            );
+
+            log(
+                `\n    see more documentation at ${color(
+                    "https://opensource.mindsphere.io/docs/mindconnect-nodejs/development-proxy.html"
+                )}\n`
+            );
         });
 };
 
-async function serve({ auth, configPort, sdk }: { auth: authJson; configPort?: number; sdk: MindSphereSdk }) {
+async function serve({
+    auth,
+    configPort,
+    sdk,
+    options,
+}: {
+    auth: authJson;
+    configPort?: number;
+    sdk: MindSphereSdk;
+    options: any;
+}) {
     const proxy = process.env.http_proxy || process.env.HTTP_PROXY;
-    log(`Proxy: ${proxy}`);
     const proxyHttpAgent = proxy ? new HttpsProxyAgent(proxy) : undefined;
 
     const server = http.createServer();
     const port = configPort || 7707;
 
+    server.on("error", (err) => {
+        console.log(`[${red(new Date().toISOString())}] ${red(err)}`);
+    });
+
     server.on("request", async (req, res: http.ServerResponse) => {
         try {
-            const options = {
+            const requestOptions = {
                 hostname: url.parse(auth.gateway).host,
                 port: 443,
                 path: req.url,
@@ -74,52 +107,87 @@ async function serve({ auth, configPort, sdk }: { auth: authJson; configPort?: n
                 agent: proxyHttpAgent,
             };
 
-            if (!req.headers["x-xsrf-token"] && !req.headers["X-XSRF-TOKEN"] && !(req.method === "GET")) {
-                console.log(
-                    `[${color(new Date().toLocaleTimeString())}] ${yellow(
-                        "WARN: x-xsrf-token is missing. the app will not work after deployment."
-                    )}`
-                );
-                console.log(
-                    `[${color(new Date().toLocaleTimeString())}] ${yellow("WARN: see")}: ${color(
-                        "https://developer.mindsphere.io/concepts/concept-authentication.html#calling-apis-from-frontend"
-                    )}`
-                );
-            }
+            !options.nowarn && addWarning(req);
 
-            options.headers.host = url.parse(auth.gateway).host;
-            (options.headers as any)["Authorization"] = `Bearer ${await sdk.GetToken()}`;
+            requestOptions.headers.host = url.parse(auth.gateway).host;
+            (requestOptions.headers as any)["Authorization"] = `Bearer ${await sdk.GetToken()}`;
 
-            const proxy = https.request(options, function (proxyres) {
-                const allHeaders = { ...headers, ...proxyres.headers };
-                if (req.method === "OPTIONS") {
+            const proxy = https.request(requestOptions, function (proxyres) {
+                const allHeaders = options.nocors ? proxyres.headers : { ...headers, ...proxyres.headers };
+                if (!options.nocors && req.method === "OPTIONS") {
                     res.writeHead(204, headers);
                     res.end();
                     return;
                 }
 
-                res.writeHead(res.statusCode, allHeaders);
-                proxyres.pipe(res, {
-                    end: true,
+                let body = "";
+
+                proxyres.on("data", (data) => {
+                    if (data) {
+                        body += data;
+                    }
+                });
+
+                proxyres.on("end", (data: any) => {
+                    if (data) {
+                        body += data;
+                    }
+
+                    let replaced = body;
+
+                    if (!options.norewrite) {
+                        const regex = new RegExp(`https://${requestOptions.hostname}`, "g");
+                        replaced = body.replace(regex, `http://localhost:${port}`);
+                        allHeaders["content-length"] = `${replaced.length}`;
+                    }
+
+                    res.writeHead(proxyres.statusCode || 500, allHeaders);
+                    res.statusCode = proxyres.statusCode || 500;
+                    res.end(replaced);
+
+                    const logColor = res.statusCode >= 200 && res.statusCode < 300 ? color : red;
+                    console.log(
+                        `[${logColor(new Date().toISOString())}] ${logColor(res.statusCode)} ${requestOptions.method} ${
+                            requestOptions.path
+                        }`
+                    );
                 });
             });
 
             req.pipe(proxy, {
                 end: true,
             });
-
-            const logColor = res.statusCode >= 200 && res.statusCode < 300 ? color : red;
-
-            console.log(
-                `${logColor(new Date().toISOString())} ${logColor(res.statusCode)} ${options.method} ${options.path}`
-            );
         } catch (error) {
-            console.log(`${error.message}`);
+            console.log(`[${red(new Date().toISOString())}] ${error.message}`);
+            res.writeHead(500, "Internal server error");
+            res.end(JSON.stringify({ error: error.message }));
         }
     });
 
     server.listen(port);
 
-    console.log(`navigate to ${color("http://localhost:" + port)} to configure the CLI`);
+    console.log(`proxy available at ${color("http://localhost:" + port)}`);
     console.log(`press ${color("CTRL + C")} to exit`);
+}
+
+function addWarning(req: any) {
+    if (!req.headers["x-xsrf-token"] && !req.headers["X-XSRF-TOKEN"] && !(req.method === "GET")) {
+        console.log(
+            `[${color(new Date().toISOString())}] ${yellow(
+                "WARN: x-xsrf-token is missing. the app will not work after deployment."
+            )}`
+        );
+        console.log(
+            `[${color(new Date().toISOString())}] ${yellow("WARN: see")}: ${color(
+                "https://developer.mindsphere.io/concepts/concept-authentication.html#calling-apis-from-frontend"
+            )}`
+        );
+    }
+}
+
+function checkRequiredParamaters(options: any) {
+    !options.passkey &&
+        throwError(
+            "you have to provide the passkey to start development proxy (see mc dev-proxy --help for more help) "
+        );
 }
