a28e0b5f75bab449823378882af34b2ff3f535c0
sn0wcat <sn0wcat@users.noreply.github.com>
2020-06-16 13:26:22 +0200

    chore: sdk exported to top level

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export * from "./api/mindconnect-agent";
 export * from "./api/mindconnect-models";
 export * from "./api/mindconnect-storage";
 export * from "./api/mindconnect-template";
+export * from "./api/sdk/index";
 export { retry } from "./api/utils";
