66fd5818233b24f3b582f790a894c76d3cc4dc66
sn0wcat <sn0wcat@users.noreply.github.com>
2020-06-16 14:49:01 +0200

    chore: credentials

--- src/api/sdk/index.ts
+++ src/api/sdk/index.ts
@@ -3,6 +3,7 @@ export * from "./agent/agent-management";
 export * from "./agent/agent-models";
 export * from "./asset/asset-management";
 export * from "./asset/asset-models";
+export * from "./common/credentials";
 export * from "./common/mindsphere-sdk";
 export * from "./common/sdk-client";
 export * from "./event/event-management";
