79a28a959bbd1c8aec5baa673487d538493c41f3
sn0wcat <sn0wcat@users.noreply.github.com>
2020-06-21 02:07:14 +0200

    chore: rollup

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import nodePolyfills from "rollup-plugin-node-polyfills";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import pkg from "./package.json";
+import { terser } from "rollup-plugin-terser";
+
+export default [
+    // browser-friendly UMD build
+    {
+        input: "dist/src/index.js",
+        output: {
+            name: "@mindconnect/mindconnect-nodejs",
+            file: pkg.browser,
+            format: "umd",
+        },
+        plugins: [
+            resolve(),
+            commonjs({ extensions: [".js", ".ts"] }), // the ".ts" extension is required
+            nodePolyfills(),
+            json(),
+            terser(),
+        ],
+    },
+];
