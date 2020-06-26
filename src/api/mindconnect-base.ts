6e5300929fdd8d5d7b9b5c4dde739b596155c42a
sn0wcat <sn0wcat@users.noreply.github.com>
2020-06-26 19:12:05 +0200

    chore: prepare for release :tada:

--- src/api/mindconnect-base.ts
+++ src/api/mindconnect-base.ts
@@ -114,7 +114,7 @@ export abstract class MindConnectBase {
     protected _headers = {
         Accept: "*/*",
         "X-Powered-By": "meowz",
-        "User-Agent": "mindconnect-nodejs (3.8.0)",
+        "User-Agent": "mindconnect-nodejs (3.8.1)",
     };
 
     /**
