a5e145ccf7b0cfe13e2121f6e04c603c4bd64b95
sn0wcat <sn0wcat@users.noreply.github.com>
2020-10-11 02:48:07 +0200

    chore: fixed lgtm

--- src/cli/commands/mc-list-assets.ts
+++ src/cli/commands/mc-list-assets.ts
@@ -49,20 +49,4 @@ export default (program: CommanderStatic) => {
         });
 };
 
-function buildFilter(options: any) {
-    const filter = (options.filter && JSON.parse(options.filter)) || {};
-    let pointer = filter;
-    if (options.assetname !== undefined && options.typeid !== undefined) {
-        filter.and = {};
-        pointer = filter.and;
-    }
-    if (options.assetname) {
-        pointer.name = { contains: `${options.assetname}` };
-    }
-    if (options.typeid) {
-        pointer.typeId = { contains: `${options.typeid}` };
-    }
-    return filter;
-}
-
 function checkRequiredParameters(options: any) {}
