8cc5ad4f62bacd1ae90bde0b1b0946bcaaffa047
sn0wcat <sn0wcat@users.noreply.github.com>
2021-05-18 23:16:37 +0000

    chore: fixed upload and ontologyjobs

--- src/api/sdk/sdi/sdi-data-template.ts
+++
@@ -1,7 +0,0 @@
-export const sdiDataTemplate = (fileName: string, buffer: Buffer, mimeType?: string) => `
-----mindsphere\r\n\
-Content-Disposition: form-data; name="file"; filename="${fileName}"\
-\r\n\
-Content-Type: ${mimeType}\
-\r\n\r\n${(buffer as Buffer).toString("ascii")}\r\n\
-----mindsphere--`;
